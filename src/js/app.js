075cac143379ac6157d23ede4b86f5b00dbdb4c9
Yevhen Lytvynchuk <evg.litik@gmail.com>
2022-07-30 12:28:40 +0300

    add-style

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,7 @@
 import fetchWeather from './app-fetch';
 import weatherIcons from './weather-icons';
 
-
 console.log(weatherIcons.toCamelCase('partly-cloudy-night'));
-
-
 
 const weatherFetch = new fetchWeather();
 
@@ -34,9 +31,7 @@ function createPageWeatherInfo(w) {
             <span>${w.currentConditions.temp}</span>
             <span>${w.currentConditions.conditions}</span>
             </div>
-
-            <div class="info-global__text-global"></div>
-
+      
             <ul class="info-global__list">
               <li class="info-global__item">
                 <p class="info-global__text">${w.currentConditions.temp} | </p>
@@ -54,32 +49,93 @@ function createPageWeatherInfo(w) {
                 } KM/H WSW</p>
               </li>
             </ul>
-          </div>
 
+            <div class="info-global__text-global"><p>${w.description}</p></div>
+
+      <ul class="weather__info--list">
+        <li class="weather__info--item">
+        <span> </span>
+          <p>Humidity ${w.currentConditions.humidity}%</p>
+        </li>
+        <li class="weather__info--item">
+        <span> </span>
+          <p>Cloudcover ${w.currentConditions.cloudcover}%</p>
+        </li>
+        <li class="weather__info--item">
+        <span> </span>
+          <p>UV-index ${w.currentConditions.uvindex}</p>
+        </li>
+        <li class="weather__info--item">
+        <span> </span>
+          <p>Visibility ${w.currentConditions.visibility}</p>
+        </li>
+      </ul>
+
+          </div>
           </div>
         </div>
       </div>`;
+
+  const nextForecast = w.days
+    .map(e => {
+      return `<li class="next-forecast__days--item">
+            <div class="next-forecast__days--header-info header-day">
+                <span class="header-day__date"> ${e.datetime}</span>
+                <span class="header-day__icon"> ${weatherIcons.toCamelCase(
+                  e.icon
+                )}</span>
+                <span class="header-day__temp"> ${e.feelslikemax} | ${
+        e.feelslikemin
+      }</span>
+            </div>
+
+            <div class="next-forecast__days--hidden-info hidden-day">
+
+              <span class="hidden-day__temp">${e.temp}</span>
+              <span class="hidden-day__icon">${weatherIcons.toCamelCase(
+                e.icon
+              )}</span>
+              <p class="hidden-day__text">${e.description}</p>
+              <ul class="hidden-day__list-info">
+                <li class="hidden-day__item-info">
+                  <span class="hidden-day__icon">XX</span>
+                  <span class="hidden-day__rain">Dew ${e.dew}</span>
+                </li>
+                <li class="hidden-day__item-info">
+                  <span class="hidden-day__icon">XX</span>
+                  <span class="hidden-day__wind">Wind ${e.windspeed} M/s</span>
+                </li>
+              </ul>
+
+              <ul class="hidden-day__list">
+                <li class="hidden-day__item">
+                  <span class="hidden-day__item--icon">XX</span>
+                  <span class="hidden-day__item--text">Humidity ${e.humidity}</span>
+                </li>
+                <li class="hidden-day__item">
+                  <span class="hidden-day__item--icon">XX</span>
+                  <span class="hidden-day__item--text">Sunrise ${e.sunrise}</span>
+                </li>
+                <li class="hidden-day__item">
+                  <span class="hidden-day__item--icon">XX</span>
+                  <span class="hidden-day__item--text">UV-index ${e.uvindex}</span>
+                </li>
+                <li class="hidden-day__item">
+                  <span class="hidden-day__item--icon">XX</span>
+                  <span class="hidden-day__item--text">Sunset ${e.sunset}</span>
+                </li>
+              </ul>
+            </div>
+          </li>`;
+    })
+    .join('');
+
   refs.sectionWeather.insertAdjacentHTML('beforeend', pageStructure);
+  refs.listNextForecastDays.insertAdjacentHTML('beforeend', nextForecast);
 }
 
 const refs = {
   sectionWeather: document.querySelector('.weather'),
+  listNextForecastDays: document.querySelector('.next-forecast__days'),
 };
-
-// function toCamelCase(string) {
-//   const arr = string.split('').map(word => {
-//     if (word === '_' || word === '-') {
-//       return '$';
-//     } else return word;
-//   });
-
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === '$') {
-//       arr.splice(i + 1, 1, arr[i + 1].toUpperCase());
-//     }
-//   }
-
-//   return arr.join('').split('$').join('');
-// }
-
-// console.log(toCamelCase('jango-her'));
+ 
